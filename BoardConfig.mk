b22b99093f215817574c19e5ddb207d7da1a6c0e
fukiame <fukiame@proton.me>
2025-03-14 16:06:05 +0700

    A37: BoardConfig: drop redundant linux host guard
    ...do people actually compile with other stuff?
    
    Signed-off-by: fukiame <fukiame@proton.me>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -80,14 +80,12 @@ TARGET_USES_MKE2FS := true
 BOARD_ROOT_EXTRA_FOLDERS := firmware persist
 
 # Dexpreopt
-ifeq ($(HOST_OS),linux)
-  ifneq ($(TARGET_BUILD_VARIANT),eng)
+ifneq ($(TARGET_BUILD_VARIANT),eng)
       WITH_DEXPREOPT := true
       WITH_DEXPREOPT_BOOT_IMG_AND_SYSTEM_SERVER_ONLY := true
       DONT_DEXPREOPT_PREBUILTS := true
       USE_DEX2OAT_DEBUG := false
       WITH_DEXPREOPT_DEBUG_INFO := false
-  endif
 endif
 
 DISABLE_APEX_TEST_MODULE := true
